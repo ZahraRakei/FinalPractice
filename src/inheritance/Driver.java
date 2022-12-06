501bdc8e719c25c3a6275b73de28c0d31830ab49
zahrarakei <zrakei.qa@gmail.com>
2022-12-06 18:35:53 -0500

    Car.java Vehicle.java Train.java Tesla.java Driver.java

--- src/inheritance/Driver.java
+++ src/inheritance/Driver.java
@@ -3,7 +3,7 @@ package inheritance;
 public class Driver {
 
     public static void main(String[] args) {
-        Car   modelS = new Tesla("white", "X", 4, true);
+        Car   modelS = new Tesla("white", "X", 4, true, 400,true, 120000, true);
         Vehicle train = new Train("black", "hg", 8, true, 300);
 
 
@@ -16,7 +16,23 @@ public class Driver {
         train.breake();
         train.accelerate();
 
-        int x = 5;
-        String name = "Sami";
+        Driver obj = new Driver();
+        Object x = obj.getTesla("white", "S", 4, true, 790, false, 80000, false);
+        if (x instanceof Vehicle){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+
+    }
+
+
+
+    public Object getTesla(String color, String model, int numOfCylinder, boolean hasRoof, int cc, boolean hasfsd, int price, boolean isLuxury){
+        Tesla tesla = new Tesla(color, model, numOfCylinder, hasRoof, cc, hasfsd, price, isLuxury);
+        return tesla;
+    }
+    public String[] getName(){
+        return new String[10];
     }
 }
